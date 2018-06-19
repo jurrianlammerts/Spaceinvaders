85dc5d5947cddb4553b8f443d20b19ff435b0747
Jurrian Lammerts <Jurrian@MacBook-Jurrian.local>
2018-06-19 10:41:31 +0200

    laser img bug

--- dev/Ship.ts
+++ dev/Ship.ts
@@ -57,7 +57,7 @@ export default class Ship extends GameObject implements Subject {
       this,
       5,
       35,
-      "../assets/images/Laser.png",
+      "../assets/images/laser.png",
       Game.getInstance().viewPort,
       true
     );
